d5a6111cdb8a442eeaa277c079cc4885ca13a49d
chiaracozzarini <chiaracozzarini@users.noreply.github.com>
2020-11-20 15:41:53 +0100

    purple

---
+++ public/sketch.js
@@ -0,0 +1,13 @@
+function preload(){
+  // put preload code here
+}
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  // put setup code here
+  background("purple"); 
+}
+
+function draw() {
+  // put drawing code here
+}
--- server.js
+++ server.js
@@ -2,7 +2,25 @@
 
 console.log("node is running");
 
-let a = 2;
+// first we have to tell the server how to send files to the client
+// moreover we have to emulate a server on our computer -> we create a virtual server, a local host
 
-a = a + 10;
-console.log(a); 
+//we add some instructions using express
+
+//we load express and we save it in a variable
+let express = require("express");
+
+// we activate the module by activating it
+let app = express();
+
+let port = 3000;
+
+let server = app.listen(port);
+
+// the past four lines are used to create a local server:
+//without further lines we get an error due to the fact the server is not sending any file to the client
+
+//we are saying to express
+app.use(express.static("public"));
+
+//we want to send p5 to our client, a sketch and index
